007061dd31ee6cd8a1496285ac9527d22ccf0f03
manovarma <manovarma2003@gmail.com>
2025-11-22 23:03:18 +0530

    Initial commit with full project files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaHackerrank } from 'react-icons/fa';
 import { AiFillMediumCircle, AiFillLinkedin } from 'react-icons/ai';
 import { FiMail } from 'react-icons/fi';
 
@@ -22,11 +22,20 @@ const Footer = () => {
                     >
                         <FiMail className='text-xl' />
                     </a>
+                    <a 
+                        href="https://www.hackerrank.com/profile/manovarmaoffici1" 
+                        target="_blank"
+                        rel="noreferrer"
+                        className="transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1"
+                    >
+                        <FaHackerrank className="text-xl" />
+                    </a>
 
                     {/* LinkedIn */}
                     <a 
                         href="https://www.linkedin.com/in/manovarma-krishnasamy-182023291/"
                         target='_blank'
+                        rel="noreferrer"
                         className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1'
                     >
                         <AiFillLinkedin className='text-xl' />
